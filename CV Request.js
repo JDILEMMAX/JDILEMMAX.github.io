158bb5bf9a9491d591a321d87196add1b9102d41
JDILEMMAX <133681614+JDILEMMAX@users.noreply.github.com>
2024-07-25 05:41:00 +0300

    CV Request.js

--- CV Request.js
+++ CV Request.js
@@ -2,11 +2,11 @@ const downloadCvButton = document.getElementById('download-cv');
 
     downloadCvButton.addEventListener('click', () => {
       // Replace 'your-cv.pdf' with the actual filename of your CV
-      const cvUrl = 'Jesse_Jacob_CV.pdf';
+      const cvUrl = 'Jesse Jacob.pdf';
 
       const link = document.createElement('a');
       link.href = cvUrl;
-      link.download = 'Jesse_Jacob_CV.pdf'; // Set the download filename
+      link.download = 'Jesse Jacob - CV.pdf'; // Set the download filename
       document.body.appendChild(link); // Append the link to the DOM
       link.click(); // Simulate a click to trigger download
       document.body.removeChild(link); // Remove the link after click
