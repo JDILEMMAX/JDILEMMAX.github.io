78bc636f270b59884a36738f0fe821b100b303fa
Jesse Jacob <jessejacobvincent@gmail.com>
2025-06-20 01:55:33 +0300

    Minor update

--- js/cv-request.js
+++ js/cv-request.js
@@ -2,11 +2,11 @@ const downloadCvButton = document.getElementById('download-cv');
 
     downloadCvButton.addEventListener('click', () => {
       // Replace 'your-cv.pdf' with the actual filename of your CV
-      const cvUrl = 'Jesse_Jacob_CV.pdf';
+      const cvUrl = 'assets/cv.pdf';
 
       const link = document.createElement('a');
       link.href = cvUrl;
-      link.download = 'Jesse_Jacob_CV.pdf'; // Set the download filename
+      link.download = "Vincent's_CV.pdf"; // Set the download filename
       document.body.appendChild(link); // Append the link to the DOM
       link.click(); // Simulate a click to trigger download
       document.body.removeChild(link); // Remove the link after click
